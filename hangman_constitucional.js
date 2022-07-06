8796b040efc16fbeb500a3fcc2aa3e7c9b24cdc7
noceive <nocivescience@gmail.com>
2022-07-06 17:45:40 -0400

    no puedo ingresar las letras

--- hangman_constitucional.js
+++ hangman_constitucional.js
@@ -12,21 +12,72 @@ const articlesEl=[
         'republica',
         '2° Se constituye como una república solidaria.'
     ]
-]
+];
 let articleIndex = Math.floor(Math.random() * articlesEl.length);
-function setTitle(title,description) {
+function showDescription(description) {
+    cardText.innerHTML = description;
+}
+showDescription(articlesEl[articleIndex][1]);
+function setTitle(title) {
     const recipient = document.createElement('h2');
     recipient.innerHTML = `
-        ${title.split('').map(letter => `<span class ='letter'>${letter}</span>`).join('')}
+        ${title.split('').map(letter => {
+            const letterRecipient = document.createElement('span');
+            letterRecipient.innerHTML = letter;
+            if(correctLetters.includes(letter)){
+                letterRecipient.classList.add('show-letter');
+            }
+    }).join('')}
     `;
     cardTitle.appendChild(recipient);
-    cardText.innerHTML = description;
     const innerWord=cardTitle.innerText.replace(/\n/g,'');
     if(innerWord===articlesEl[articleIndex][0]){
         notificationEl.innerHTML = '<span class="badge badge-success">Correcto Apruebo</span>';
     }
 }
-function updateWrongletter(){
-
-}
-setTitle(articlesEl[articleIndex][0],articlesEl[articleIndex][1]);
+function showNotification(message) {
+    notificationEl.classList.add('show');
+    setTimeout(() => {
+        notificationEl.classList.remove('show');
+    }, 2000);
+};
+window.addEventListener('keydown', e => {
+    let letter=e.key;
+    if(
+        letter==='a'||
+        letter==='b'||
+        letter==='c'||
+        letter==='d'||
+        letter==='e'||
+        letter==='f'||
+        letter==='g'||
+        letter==='h'||
+        letter==='i'||
+        letter==='j'||
+        letter==='k'||
+        letter==='l'||
+        letter==='m'||
+        letter==='n'||
+        letter==='o'||
+        letter==='p'||
+        letter==='q'||
+        letter==='r'||
+        letter==='s'||
+        letter==='t'||
+        letter==='u'||
+        letter==='v'||
+        letter==='w'||
+        letter==='x'||
+        letter==='y'||
+        letter==='z'
+    ){
+        if(articlesEl[articleIndex][0].includes(letter)&&!correctLetters.includes(letter)){
+            correctLetters.push(letter);
+            setTitle(articlesEl[articleIndex][0],articlesEl[articleIndex][1]);
+        }else{
+            wrongLetters.push(letter);
+            showNotification(`${letter} no es una letra`);
+        }
+    }
+});
+setTitle(articlesEl[articleIndex][0]);
