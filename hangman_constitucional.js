bc9ed3c40f761b3f4bfc9f06669110b2680896bd
noceive <nocivescience@gmail.com>
2022-07-06 15:46:06 -0400

    corre hasta el momento agregue solo las listas

--- hangman_constitucional.js
+++ hangman_constitucional.js
@@ -1,20 +1,32 @@
 const cardTitle = document.querySelector('#card-title');
 const cardText = document.querySelector('.card-text');
+const notificationEl = document.querySelector('.notification');
+const correctLetters=[];
+const wrongLetters=[];
 const articlesEl=[
     [
-        ['democratico'],
-        ['1° Chile es un Estado social y democrático de derecho. Es plurinacional, intercultural, regional y ecológico.']
+        'democratico',
+        '1° Chile es un Estado social y democrático de derecho. Es plurinacional, intercultural, regional y ecológico.'
     ],
     [
-        ['republica'],
-        ['2° Se constituye como una república solidaria.']
+        'republica',
+        '2° Se constituye como una república solidaria.'
     ]
 ]
 let articleIndex = Math.floor(Math.random() * articlesEl.length);
 function setTitle(title,description) {
     const recipient = document.createElement('h2');
-    recipient.innerHTML = title;
+    recipient.innerHTML = `
+        ${title.split('').map(letter => `<span class ='letter'>${letter}</span>`).join('')}
+    `;
     cardTitle.appendChild(recipient);
     cardText.innerHTML = description;
+    const innerWord=cardTitle.innerText.replace(/\n/g,'');
+    if(innerWord===articlesEl[articleIndex][0]){
+        notificationEl.innerHTML = '<span class="badge badge-success">Correcto Apruebo</span>';
+    }
+}
+function updateWrongletter(){
+
 }
 setTitle(articlesEl[articleIndex][0],articlesEl[articleIndex][1]);
